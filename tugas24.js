1c4fed403400be26e63d578a8ac668559af00b1e
dnriski <nugrohoriski159@gmail.com>
2023-02-09 18:46:10 +0700

    first commit

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log(`Sebelumnya : ${angka}`);
+
+var hasil = angka.sort();
+console.log(`Ascending :  ${hasil}`);
+
+var result2 = angka.reverse();
+console.log(`Descending :  ${result2}`);
